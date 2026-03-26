4e1ede25a444c10b4a3138a8a5c79e8b112418cc
Banence <philip.prestigio@gmail.com>
2026-03-26 10:11:04 +0200

    Initial Commit

---
+++ Weep/WeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeepApp.swift
+//  Weep
+//
+//  Created by Filip 'Big G' Angelov on 26.03.26.
+//
+
+import SwiftUI
+
+@main
+struct WeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
